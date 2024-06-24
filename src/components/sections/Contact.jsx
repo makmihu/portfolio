88bb327d5c68e1572e1203d1b8630a0271d5729e
MaKayla Mihu <>
2024-06-24 15:27:26 -0600

    Adding Resume

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -6,7 +6,8 @@ export default function Contact(props){
       <h1>Contact Info</h1>
         
       <p>Email: makmihu@gmail.com</p>
-      <p>LinkedIn:<a href="https://www.linkedin.com/in/makayla-mihu/" target="_blank">linkedin.com/in/makayla-mihu/</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/makayla-mihu/" target="_blank">linkedin.com/in/makayla-mihu/</a></p>
+      <p>View My Resume: <a href="https://docs.google.com/document/d/e/2PACX-1vTs3teHRAj1hpzxUArYZgVTx6ubabnWh4CQI6Sc1XvtxKFWlVd49ZUmCqlgb-_0StooP6ZhTfwSGQls/pub">Resume 2024</a></p>
     </div>
   )
 }
