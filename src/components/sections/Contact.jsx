6920479090a4f99bc6c4a05a4d62f72c93faa544
MaKayla Mihu <makmihu@gmail.com>
2024-12-15 16:01:12 -0700

    adding figma link

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -7,7 +7,8 @@ export default function Contact(props){
         
       <p>Email: makmihu@gmail.com</p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/makayla-mihu/" target="_blank">linkedin.com/in/makayla-mihu/</a></p>
-      <p>View My Resume: <a href="https://docs.google.com/document/d/e/2PACX-1vTs3teHRAj1hpzxUArYZgVTx6ubabnWh4CQI6Sc1XvtxKFWlVd49ZUmCqlgb-_0StooP6ZhTfwSGQls/pub">Resume 2024</a></p>
+      <p>View My Resume: <a href="https://docs.google.com/document/d/1-YXz7E367nEIHiXC_TiElzNeFC1sE0wH7VJGmRv6v6w/edit?usp=sharing">Resume 2024</a></p>
+      <p>See my Figma Design: <a href="https://www.figma.com/design/FsPKaxbOOgtqYy6ByLXR38/Mikkie-Makes?node-id=0-1&t=3dxNV3hhz4LvWWIu-1" target="_blank">Mikkie Makes Figma Design</a></p>
     </div>
   )
 }
